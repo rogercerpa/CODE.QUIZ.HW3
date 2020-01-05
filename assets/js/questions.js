1ab167ed3d28373a52d455de7bd4aa5a0cb01f3b
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-04 20:29:54 -0500

    created the question variable and saved it on a separate js file

---
+++ assets/js/questions.js
@@ -0,0 +1,50 @@
+// questions source :
+//http://mcqspdfs.blogspot.com/2013/08/60-top-javascript-multiple-choice.html
+
+var questions = [
+	{
+		question      :
+			'JavaScript entities start with _______ and end with _________.',
+		answeroptions : [
+			'A.  Semicolon, colon',
+			'B.   Semicolon, Ampersand',
+			'C.  Ampersand, colon',
+			'D.  Ampersand, semicolon'
+		],
+		answer        : 'D'
+	},
+	{
+		question      : 'Which of the following best describes JavaScript?',
+		answeroptions : [
+			'A.  a low-level programming language.',
+			'B.   a scripting language precompiled in the browser.',
+			'C.  a compiled scripting language.',
+			'D.  an object-oriented scripting language.'
+		],
+		answer        : 'D'
+	},
+
+	{
+		question      :
+			'Which of the following is not considered a JavaScript operator?',
+		answeroptions : [ 'A.  new', 'B.  this', 'C.  delete', 'D.  typeof' ],
+		answer        : 'B'
+	},
+
+	{
+		question      : ' Is it possible to nest functions in JavaScript?',
+		answeroptions : [ 'A. True', 'B. False' ],
+		answer        : 'A'
+	},
+
+	{
+		question      : "What is mean by 'this' keyword in javascript?",
+		answeroptions : [
+			'A. It refers current object',
+			'B. It referes previous object',
+			'C. It is variable which contains value',
+			'D. None of the above'
+		],
+		answer        : 'A'
+	}
+];
